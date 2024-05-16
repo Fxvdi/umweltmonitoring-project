60e29e7550f78f838eea556d5ed80645a21eda6a
Achim Duong <130672472+QMoWo@users.noreply.github.com>
2024-05-16 11:43:42 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ import requests
 #everything about styling
 attr_style = '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 url_style = 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png'
+
+custom_icon = dict(
+    iconUrl='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f535.png',
+    iconSize=[10, 10]
+)
 #---------------------------------------------------------
 #constants
 latitude = 52.516181 #Breitengrad
@@ -20,7 +25,7 @@ coordinates = {"id": [],
                 }
 df_coordinates = pd.DataFrame(coordinates)
 
-url = "https://api.opensensemap.org/boxes?near=10.323007,51.16501&maxDistance=100000&exposure=outdoor&title=PM10,PM2.5&format=geojson"
+url = "https://api.opensensemap.org/boxes?near=10.323007,51.16501&maxDistance=200000&exposure=outdoor&title=PM10,PM2.5&format=geojson"
 data = requests.get(url).json()
 for idx in range(0, len(data["features"])):
     df2 = {"id": data["features"][idx]["properties"]["_id"], "latitude": data["features"][idx]["geometry"]["coordinates"][1], "longitude": data["features"][idx]["geometry"]["coordinates"][0]}
@@ -38,31 +43,18 @@ app.layout = html.Div([
             dl.TileLayer(url=url_style, attribution=attr_style),
             *[
                 dl.Marker(
-                    position=[row["latitude"], row["longitude"]], children=[dl.Popup(content=f"BoxID: {row['id']}")]
+                    position=[row["latitude"], row["longitude"]], children=[dl.Popup(content=f"BoxID: {row['id']}")],
+                    icon = dict(
+                        iconUrl='https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f535.png',
+                        iconSize=[10, 10])
                 )for index, row in df_coordinates.iterrows()
             ]
         ], center=[51.16501, 10.323007], zoom=7, style={"height": "65vh"}, id="boxes-in-germany")
+    ]),
+    html.Div([
+        "Live-Tracking BoxID: ",
+        dcc.Input(id="my-input", type="text")
     ])
 ])
-
-# @callback(
-# Output("interactive-map", "children"),
-# Input()
-# )
-# def update_map():
-#     updated_map = [
-#         dl.TileLayer(url=url_style, attribution=attr_style),
-#         *[
-#             dl.Marker(
-#                 position=[row["latitude"], row["longitude"]],
-#                 children=[
-#                     dl.Popup([
-#                         html.P(f"BoxID: {row['id']}"),
-#                     ])
-#                 ]
-#             )for index, row in df_coordinates.iterrows()
-#         ]
-#     ]
-#     return updated_map
 if __name__=="__main__":
     app.run_server(debug=True)
